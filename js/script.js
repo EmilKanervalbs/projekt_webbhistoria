35542b374358a060656d13e8c037ada62d701dfc
EmilKanervalbs <54535838+EmilKanervalbs@users.noreply.github.com>
2019-12-02 15:45:07 +0100

    bilder delvis fixade

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-if (localStorage.getItem("theme") != "dark" && localStorage.getItem("theme") != "light") {
+
+if (localStorage.getItem("theme") != "dark" && localStorage.getItem("theme") != "light") { //checkar om man har varit på sidan tidigare eller har valt ett tema, om inte sätter till dark
     localStorage.setItem("theme", "dark");
     console.log("satte theme till dark för hittade inte");
 }
 
-var theme = localStorage.getItem("theme");
-var currentTheme = "dark";
+var theme = localStorage.getItem("theme"); //kollar upp vilket tema man har valt tidigare
+var currentTheme = "dark"; //sätter nuvarande temat till dark
 
-console.log(localStorage.getItem("theme"));
+console.log(localStorage.getItem("theme")); //skriver ut temat
 
-function setDark() {
+function setDark() { //byter tema till mörkt
+    //letar upp alla element som behöver få sitt tema bytt
     var main = document.getElementsByClassName("light");
 
     var list = document.getElementsByClassName("nb-light");
 
     var content = document.getElementsByClassName("content-light");
 
-
-    console.log(list.length  + " " + main.length)
-
+    //går igenom alla element som finns i listorna 
     for (let i = 0; i < main.length;) {
         main[0].classList.add("dark");
         main[0].classList.remove("light");
@@ -31,8 +31,6 @@ function setDark() {
     for (let i = 0; i < content.length;) {
         content[0].classList.add("content-dark");
         content[0].classList.remove("content-light");
-        console.log("main " + content.length);
-        console.log("hej");
     }
 
     localStorage.setItem("theme", "dark");
@@ -47,36 +45,29 @@ function setLight() {
     var list = document.getElementsByClassName("nb-dark"); 
     
     var content = document.getElementsByClassName("content-dark");
-    // console.log(list.length  + " " + main.length)
-
-    // console.log(main.length);
 
     for (let i = 0; i < main.length;) {
         main[0].classList.add("light");
         main[0].classList.remove("dark");
-        console.log("main " + main.length);
-        console.log("hej");
     }
 
     for (let i = 0; i < list.length;) {
         list[0].classList.add("nb-light");
         list[0].classList.remove("nb-dark");
-        console.log("list " + list.length);
     }
 
     for (let i = 0; i < content.length;) {
         content[0].classList.add("content-light");
         content[0].classList.remove("content-dark");
-        console.log("main " + content.length);
-        console.log("hej");
     }
 
+    console.log("Blind mode activated");
 
     localStorage.setItem("theme", "light");
     currentTheme = "light";
     theme = "light";
 }
-// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
 function changeTheme() {
     if (theme == "light") {
         setDark();
